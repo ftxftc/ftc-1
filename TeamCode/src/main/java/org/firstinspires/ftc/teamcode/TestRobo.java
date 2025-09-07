7e2fc75b2347d09387cc4d0e076da00c45b23520
Alpha <brightscholars@gmail.com>
2025-09-07 08:46:06 -0500

    First file change

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRobo.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+//Added first comment to this file for checking in to github
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "FourWheelRun")
+public class TestRobo extends LinearOpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        // Reset encoders
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Reverse the right motors only (typical drivetrain config)
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        int targetTicks = 1000;  // distance to move forward
+        double speed = 0.5;
+
+        // All motors move same direction for forward movement
+        frontLeft.setTargetPosition(targetTicks);
+        frontRight.setTargetPosition(targetTicks);
+        backLeft.setTargetPosition(targetTicks);
+        backRight.setTargetPosition(targetTicks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+
+        // Wait until motors reach the target
+        while (opModeIsActive() &&
+                (frontLeft.isBusy() || frontRight.isBusy() ||
+                        backLeft.isBusy() || backRight.isBusy())) {
+            idle();
+        }
+
+        // Stop motors
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
